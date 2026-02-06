9b699caf2e6b8a04a029f56b4ff0a8f4e89a79e8
小草爸[爆竹] <oncwnuJS9X27CUG19B-jUgkunmpE@git.weixin.qq.com>
2026-02-06 15:17:43 +0800

    移除__init__.py中的插件导入，插件改为动态加载

--- worktools/plugins/__init__.py
+++ worktools/plugins/__init__.py
@@ -1,24 +1,10 @@
 """
 插件包初始化文件
 用于管理和注册所有功能插件
+
+注意：插件现在动态加载，不在此处导入
+插件文件应存储在用户目录中：
+- Windows: %APPDATA%/WorkTools/plugins
+- Mac: ~/Library/Application Support/WorkTools/plugins
+- Linux: ~/.config/WorkTools/plugins
 """
-
-# 导入所有插件类，以便插件管理器可以发现它们
-from .plugin1 import TextProcessor
-from .plugin2 import FileManager
-from .plugin3 import SystemTools
-from .monthly_summary import MonthlySummary
-from .excel_merger import ExcelMerger
-from .excel_deduplication import ExcelDeduplication
-from .image_watermark import ImageWatermarkPlugin
-
-# 插件列表
-__all__ = [
-    'TextProcessor',
-    'FileManager', 
-    'SystemTools',
-    'MonthlySummary',
-    'ExcelMerger',
-    'ExcelDeduplication',
-    'ImageWatermarkPlugin'
-]
